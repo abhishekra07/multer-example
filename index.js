8fb99932017c2d35844d47f36bfef56196de5c76
abhishekra07 <40576705+abhishekra07@users.noreply.github.com>
2022-11-10 12:47:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,13 +4,23 @@ const bodyParser = require('body-parser');
 
 const multer  = require('multer')
 
-const upload = multer({ dest: 'uploads/' })
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, '/tmp/my-uploads')
+    },
+    filename: function (req, file, cb) {
+        // const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+        // cb(null, file.originalname + '-' + uniqueSuffix)
+        cb(null, file.originalname)
+    }
+})
+
+const upload = multer({ storage: storage })
 
 const port = 3000
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
 
 app.post('/upload-single-file', upload.single(), function (req, res, next) {
     console.log(req.file);
